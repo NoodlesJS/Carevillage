6cfcd83b3e799d63d8d320a66a799c071959e2dd
NoodlesJS <romaenwritescode@gmail.com>
2020-02-02 15:44:18 -0600

    added server and tests

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.use(express.static('public'));
 
+app.use(morgan('common'));
+app.use(express.static('public'));
 app.get('/', function (req, res) {
     
 });
-app.listen(process.env.PORT || 8080);
 
-module.exports = app;
+
+// SERVER
+let server;
+
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app
+        .listen(port, () => {
+            console.log(`Server running on PORT ${port}`);
+            resolve(server);
+        })
+        .on('error', err => {
+            reject(err);
+        })
+    })
+};
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log("Closing server");
+        server.close(err => {
+            if(err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        })
+        
+    });
+};
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
--- test/test.js
+++ test/test.js
@@ -1,12 +1,20 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const app = require('../server');
+const { app, runServer, closeServer }= require('../server');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Status code', function() {
+    before(function() {
+        return runServer();
+    })
+    after(function() {
+        return closeServer();
+    })
+
+    
     it('Should give status 200 when GET runs', function() {
         return chai.request(app)
         .get('/')
