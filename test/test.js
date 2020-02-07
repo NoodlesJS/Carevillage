5c9667d2e0c20fa4ef50771611f1199eb589690d
NoodlesJS <romaenwritescode@gmail.com>
2020-02-06 21:46:45 -0600

    tests removed for now

--- test/test.js
+++ test/test.js
@@ -6,20 +6,3 @@ const { app, runServer, closeServer }= require('../server');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('Status code', function() {
-    before(function() {
-        return runServer();
-    })
-    after(function() {
-        return closeServer();
-    })
-
-    
-    it('Should give status 200 when GET runs', function() {
-        return chai.request(app)
-        .get('/')
-        .then(function(res) {
-            expect(res).to.have.status(200);
-        })
-    })
-})
